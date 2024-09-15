469394becdf2256f71e5dcdaed03eb9e6422cb1c
snubwoody <wakunguma13@gmail.com>
2024-09-14 18:31:49 -0600

    fixed the input overflox

--- frontend/src/lib/components/input.svelte
+++ frontend/src/lib/components/input.svelte
@@ -11,3 +11,10 @@
 	<Text weight="medium">{label}</Text>
 	<input {type} {placeholder} {name} class="px-4 py-3 rounded-2 border border-neutral-400 focus:border-primary-dark outline-none">
 </label>
+
+<style lang="postcss">
+	input{
+		box-sizing: border-box;
+		width: 100%;
+	}
+</style>
