b44d36266d7f92cbdb124ac3dc105a94b2007ac9
waku <wakunguma13@gmail.com>
2024-09-23 14:15:21 -0600

    removed all traces of the other old carts

--- frontend/src/lib/components/cart.svelte
+++ frontend/src/lib/components/cart.svelte
@@ -2,7 +2,7 @@
     import { XIcon,MinusIcon,PlusIcon } from "svelte-feather-icons";
 	import Button from "./button.svelte";
     import Text from "./text.svelte";
-    import { Cart, createCart,cart } from "$lib/cart.svelte";
+    import { createCart,cart } from "$lib/cart.svelte";
     import { currency } from "$lib/lib";
     import { goto } from "$app/navigation";
 
