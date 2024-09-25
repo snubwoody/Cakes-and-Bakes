fd1fe30351c66715457300babfe47fe096761304
waku <wakunguma13@gmail.com>
2024-09-24 22:39:24 -0600

    last commit before merge

--- frontend/src/lib/components/button.svelte
+++ frontend/src/lib/components/button.svelte
@@ -24,12 +24,6 @@
 		...others
 	}:Props = $props()
 
-	const buttonStyle = {
-		primary:'bg-primary-action hover:shadow-lg text-neutral-100',
-		bordered:'border border-neutral-400 hover:bg-neutral-200 text-neutral-900',
-		neutral:'bg-neutral-900 text-neutral-100 hover:bg-neutral-800'
-	}
- 
 	const buttonSize = fit ? 'w-fit' : 'w-full'
 	
 </script>
