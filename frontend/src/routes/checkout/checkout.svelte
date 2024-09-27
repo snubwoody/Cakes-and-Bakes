f38e1697707b8db79f362892f6b6eaee75822428
waku <wakunguma13@gmail.com>
2024-09-26 20:57:01 -0600

    fixed missing function error

--- frontend/src/routes/checkout/checkout.svelte
+++ frontend/src/routes/checkout/checkout.svelte
@@ -44,7 +44,7 @@
 		</div>
 		<Divider/>
 		<div class="flex flex-col">		
-			<Input required oninput={maskDate} name="date" type="date" label="Pick up date" placeholder="DD / MM / YYYY"/>
+			<Input required name="date" type="date" label="Pick up date" placeholder="DD / MM / YYYY"/>
 		</div>
 		<Button type="submit" onclick={addPurchase}>Confirm purchase</Button>
 	</form>
